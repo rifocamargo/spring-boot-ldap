9a43ad8248075798bd9c115669a934b2039c9bee
rifocamargo <rifocamargo@gmail.com>
2024-08-16 09:31:04 -0300

    Add files via upload

---
+++ src/main/java/com/memorynotfound/ldap/Application.java
@@ -0,0 +1,33 @@
+package com.memorynotfound.ldap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@SpringBootApplication
+public class Application {
+
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @PostConstruct
+    public void setup(){
+        log.info("Spring LDAP + Spring Boot Configuration Example");
+
+        List<String> names = personRepository.getAllPersonNames();
+        log.info("names: " + names);
+
+        System.exit(-1);
+    }
+
+}
---
+++ src/main/java/com/memorynotfound/ldap/PersonRepository.java
@@ -0,0 +1,33 @@
+package com.memorynotfound.ldap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ldap.core.AttributesMapper;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.stereotype.Service;
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import java.util.List;
+import static org.springframework.ldap.query.LdapQueryBuilder.query;
+
+@Service
+public class PersonRepository {
+
+    @Autowired
+    private LdapTemplate ldapTemplate;
+
+    /**
+     * Retrieves all the persons in the ldap server
+     * @return list of person names
+     */
+    public List<String> getAllPersonNames() {
+        return ldapTemplate.search(
+                query().where("objectclass").is("person"),
+                new AttributesMapper<String>() {
+                    public String mapFromAttributes(Attributes attrs)
+                            throws NamingException {
+                        return (String) attrs.get("cn").get();
+                    }
+                });
+    }
+
+}
